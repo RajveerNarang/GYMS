f279c2079e7390994cc13de6772868ea43a001bd
RajveerNarang <dhanurrajveer@gmail.com>
2025-05-12 09:51:36 +0530

    deploy on vercel 3 config change

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import {
   doc,
@@ -10,7 +13,10 @@ import {
   onValue,
 } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
 
-import { app, auth } from "./firebase-config.js";
+// import { app, auth } from "./firebase-config.js";
+
+const app = initializeApp(window.firebaseConfig);
+const auth = getAuth(app);
 
 const db = getFirestore(app);
 
