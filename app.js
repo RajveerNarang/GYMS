30737a086fc2c7cb06dca1505679135da8662119
RajveerNarang <dhanurrajveer@gmail.com>
2025-05-12 09:53:30 +0530

    firestore redec removed

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import {
   doc,
