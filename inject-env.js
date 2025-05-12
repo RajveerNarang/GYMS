a9522efcd7015112848069a34d1ca9a1b61f2a3c
RajveerNarang <dhanurrajveer@gmail.com>
2025-05-12 10:22:12 +0530

    vercel json fix 6

--- inject-env.js
+++ inject-env.js
@@ -1,18 +1,18 @@
 const fs = require("fs");
 
 const env = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
 };
 
 let html = fs.readFileSync("index.html", "utf8");
 
 Object.keys(env).forEach((key) => {
-  const placeholder = `__${key.toUpperCase()}__`;
+  const placeholder = `__${key}__`;
   html = html.replaceAll(placeholder, env[key]);
 });
 
